4ac62bea5477607918e80151273566ab77452e5e
Borys Minaiev <mb.787788@gmail.com>
2018-06-04 01:15:49 +0300

    Number bases

--- src/NumberBasesSol.java
+++ src/NumberBasesSol.java
@@ -1,7 +1,4 @@
-import java.math.BigInteger;
-import java.util.*;
-
-public class Main {
+public class NumberBasesSol {
     final boolean debug = false;
 
     // [partFrom .. partTo)
@@ -227,6 +224,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        new Main().start();
+        new NumberBasesSol().start();
     }
 }
