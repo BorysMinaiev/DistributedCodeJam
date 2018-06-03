8d3324dd15a8608d60aaa9d2c1202796711d0750
Borys Minaiev <mb.787788@gmail.com>
2018-06-04 01:35:04 +0300

    Number bases

--- src/PancakesSol.java
+++ src/PancakesSol.java
@@ -1,7 +1,4 @@
-import java.math.BigInteger;
-import java.util.*;
-
-public class Main {
+public class PancakesSol {
     final boolean debug = false;
 
     // [partFrom .. partTo)
@@ -153,6 +150,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        new Main().start();
+        new PancakesSol().start();
     }
 }
